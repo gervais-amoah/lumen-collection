f97db2b7810f68241372c7380dff638aec3dde2b
Yao Gervais Amoah <gervaisamoah@yahoo.com>
2026-01-27 15:23:19 +0000

    Use algolia chat on assistance mode

--- app/(assistant)/page.tsx
+++ app/(assistant)/page.tsx
@@ -1,31 +1,18 @@
 "use client";
 
 import DarkVeil from "@/components/animation/dark-veil";
-import { ChatPanel } from "@/components/assistant/chat-panel";
-import { WelcomeMessage } from "@/components/assistant/welcome-message";
+import AlgoliaChat from "@/components/assistant/algolia-chat";
 import DemoInfoDialog from "@/components/common/demo-dialog";
-import { ProductGrid } from "@/components/common/product-grid";
 import FluidGlass from "@/components/ui/fluid-glass";
-import { useProductStore } from "@/store/useProductStore";
 
 export default function WelcomePage() {
-  const products = useProductStore((state) => state.products);
-
   return (
-    <div className="flex flex-1 overflow-hidden h-[calc(100%-120px)] max-w-[1320px] mx-auto mt-30">
-      {/* Left Panel */}
-      <div className="w-[calc(100%-500px)] relative p-4 pt-0 h-full">
-        {products.length === 0 ? (
-          <WelcomeMessage />
-        ) : (
-          <ProductGrid products={products} />
-        )}
-      </div>
-
-      {/* Right Chat Panel */}
-      <div className="w-[500px] max-w-2xl flex flex-col pr-6 pb-6">
-        <FluidGlass className=" rounded-2xl h-full">
-          <ChatPanel />
+    <div className="overflow-hidden h-[calc(100%-120px)] w-full max-w-[1320px] mx-auto mt-30">
+      {/* Main Chat Panel */}
+      <div className="flex pb-6 w-4/5 h-full mx-auto">
+        <FluidGlass className="w-full rounded-2xl h-full">
+          {/* <ChatPanel /> */}
+          <AlgoliaChat />
         </FluidGlass>
       </div>
 
---
+++ components/assistant/algolia-chat.tsx
@@ -0,0 +1,130 @@
+import { Product } from "@/types/product";
+import { liteClient as algoliasearch } from "algoliasearch/lite";
+import Image from "next/image";
+import { Chat, Configure, InstantSearch } from "react-instantsearch";
+import ShinyText from "../animation/shiny-text";
+import { ProductCard } from "../common/product-card";
+import "../instantsearch.css/components/chat.scss";
+
+const agentAppId = process.env.NEXT_PUBLIC_ALGOLIA_AGENT_APP_ID!;
+const agentApiKey = process.env.NEXT_PUBLIC_ALGOLIA_AGENT_API_KEY!;
+const agentId = process.env.NEXT_PUBLIC_ALGOLIA_AGENT_ID!;
+const agentIndexName = process.env.NEXT_PUBLIC_ALGOLIA_AGENT_INDEX_NAME!;
+
+const searchClient = algoliasearch(agentAppId, agentApiKey);
+
+export default function AlgoliaChat() {
+  return (
+    /* We are using the [&_*] selector which targets EVERY child element 
+       inside this div to reset any restrictive Algolia defaults.
+    */
+    <div
+      className="relative h-full w-full
+        **:transform-none! 
+        **:transition-none! 
+        **:opacity-100! 
+        **:pointer-events-auto!
+        
+        [&_.ais-Chat]:static! 
+        [&_.ais-Chat]:h-full! 
+        [&_.ais-Chat]:w-full!
+        [&_.ais-Chat]:max-h-full!
+        [&_.ais-Chat]:max-w-full!
+        [&_.ais-Chat]:flex 
+        [&_.ais-Chat]:flex-col
+        
+        [&_.ais-Chat-window]:h-full! 
+        [&_.ais-Chat-window]:shadow-none! 
+        [&_.ais-Chat-window]:static!
+        
+        [&_.ais-ChatHeader]:border-b!
+        [&_.ais-ChatHeader]:border-white/20!
+
+        [&_.ais-Chat-container]:static!
+        [&_.ais-Chat-container]:h-full!
+
+        [&_.ais-Chat-container]:bg-transparent!
+        
+        [&_.ais-Chat-toggleButtonWrapper]:hidden!
+        
+        [&_.ais-ChatPrompt]:before:content-none! 
+        [&_.ais-ChatHeader]:after:content-none!
+
+        [&_.ais-ChatMessage-message_.ais-Carousel]:before:content-none! 
+        [&_.ais-ChatMessage-message_.ais-Carousel]:after:content-none!
+        "
+    >
+      <InstantSearch searchClient={searchClient} indexName={agentIndexName}>
+        {/* Adding 'isolate' helps create a clean stacking context 
+            where children are preferred for clicks. 
+        */}
+        <div className="h-full w-full flex flex-col isolate">
+          <Configure
+            hitsPerPage={10}
+            attributesToRetrieve={[
+              "name",
+              "description",
+              "brand",
+              "category",
+              "size",
+              "material",
+              "price",
+              "occasion",
+              "style",
+              "color",
+              "objectID",
+              "image_url",
+            ]}
+            attributesToSnippet={["name", "description"]}
+            snippetEllipsisText="…"
+          />
+
+          <Chat
+            agentId={agentId}
+            title="Maya • Assistant Mode"
+            translations={{
+              header: {
+                title: "Maya • Assistant Mode",
+                closeLabel: "Close chat",
+              },
+              messages: {
+                loaderText: "Maya is thinking...", // Fallback text
+              },
+            }}
+            itemComponent={({ item }) => (
+              <ProductCard
+                product={item as unknown as Product}
+                index={Number()}
+              />
+              // {ProductCard}
+            )}
+            classNames={{
+              header: {
+                close: "hidden!",
+                maximize: "hidden!",
+              },
+            }}
+            // Other props to further customize the chat can be added here
+            // headerCloseIconComponent={() => <div />}
+            // headerMaximizeIconComponent={() => <div />}
+            headerMinimizeIconComponent={() => <div />}
+            headerTitleIconComponent={() => (
+              <Image
+                src="/images/assistant_avatar.jpg"
+                alt="Assistant Avatar"
+                className=" rounded-4xl"
+                width={35}
+                height={35}
+              />
+            )}
+            // messagesErrorComponent={function}
+            messagesLoaderComponent={() => (
+              <ShinyText text="Curating some picks you'll love..." />
+            )}
+            // toggleButtonIconComponent={function}
+          />
+        </div>
+      </InstantSearch>
+    </div>
+  );
+}
