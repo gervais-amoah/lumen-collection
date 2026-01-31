220ced8f18a24512b0b58914b429fdad8101ddf6
Yao Gervais Amoah <gervaisamoah@yahoo.com>
2026-01-31 00:54:24 +0000

    fetch products from new index

--- lib/get-products.ts
+++ lib/get-products.ts
@@ -1,13 +1,16 @@
 // /lib/get-products
 import { supabase } from "./supabase";
 
+const LEGACY_PRODUCTS_KEY = "products";
+const AGENT_PRODUCTS_KEY = "agent_assisted_products";
+
 export async function fetchProducts(filters: {
   category?: string;
   type?: string;
   color?: string;
   limit?: number;
 }) {
-  let query = supabase.from("products").select("*");
+  let query = supabase.from(AGENT_PRODUCTS_KEY).select("*");
 
   if (filters.category) query = query.eq("category", filters.category);
   if (filters.type) query = query.eq("type", filters.type);
