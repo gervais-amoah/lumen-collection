b5c1809d20044a719086dc885d3f1b95fb9ec0ee
Yao Gervais Amoah <gervaisamoah@yahoo.com>
2026-01-31 00:53:44 +0000

    remove chatbot from classic mode

--- app/(classic)/layout.tsx
+++ app/(classic)/layout.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 // app/classic/layout.tsx
-import { ChatSearchItem } from "@/components/algolia-search/ChatSearchItem";
-import { ALGOLIA_CONFIG, searchClient } from "@/lib/algolia";
-import { AlgoliaProduct } from "@/types/product";
-import { Inter } from "next/font/google";
-import React from "react";
-import { Chat, Configure, InstantSearch } from "react-instantsearch";
-import "../../components/instantsearch.css/components/chat.scss";
 import DemoInfoDialog from "@/components/common/demo-dialog";
+import { Inter, Playfair_Display } from "next/font/google";
+import React from "react";
+import "../../components/instantsearch.css/components/chat.scss";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+});
 
 export default function ClassicLayout({
   children,
@@ -18,55 +18,13 @@ export default function ClassicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-linear-to-br from-purple-950 to-gray-950 text-gray-100 font-sans pt-22 pb-12 min-h-screen h-full ">
+    <div className="bg-linear-to-br  from-gray-900 via-gray-800 to-black text-gray-100 font-sans pt-22 pb-12 min-h-screen h-full ">
       {/* Main content with padding to avoid header overlap */}
       <main
-        className={`${inter.variable} w-[calc(100%-40px)] lg:w-4/5 max-w-7xl mx-auto `}
+        className={`${inter.variable} ${playfair.variable} w-[calc(100%-40px)] lg:w-4/5 max-w-7xl mx-auto `}
       >
         {children}
       </main>
-
-      <div>
-        <InstantSearch
-          searchClient={searchClient}
-          indexName={ALGOLIA_CONFIG.agentIndexName}
-        >
-          <Configure
-            hitsPerPage={10}
-            attributesToRetrieve={[
-              "name",
-              "description",
-              "brand",
-              "category",
-              "size",
-              "material",
-              "price",
-              "occasion",
-              "style",
-              "color",
-              "objectID",
-              "image_url",
-            ]}
-            attributesToSnippet={["name", "description"]}
-            snippetEllipsisText="…"
-          />
-          <Chat
-            agentId={ALGOLIA_CONFIG.agentId}
-            title="Maya • Chat"
-            translations={{
-              header: {
-                title: "Maya • Chat",
-                closeLabel: "Close chat",
-                // ...
-              },
-              // ...
-            }}
-            itemComponent={({ item }) => (
-              <ChatSearchItem item={item as unknown as AlgoliaProduct} />
-            )}
-          />
-        </InstantSearch>
-      </div>
 
       <footer>
         <div className="w-full py-6 px-6 lg:px-48 border-t border-gray-800 text-sm text-gray-500">
